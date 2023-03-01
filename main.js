28325dc1bd030c9b594af200f6269f2919843462
norslab <mirjalolrahmonqulov4@gmail.com>
2023-03-01 11:17:30 +0500

    aaa

---
+++ main.js
@@ -0,0 +1,1148 @@
+// let counter = 0;
+
+// for(let i = 100; i < 1000; i++){
+//   if (i % 45 === 0 ) {
+//      counter++
+//   }
+// }
+// console.log(counter);
+
+
+
+
+
+
+
+// let son = promt('Son kiritng');
+
+// for (let son = 0; son < array.length; son++) {
+
+// }
+
+// let n = 360;
+// let sum = 0;
+
+// for(let i = n; i > 0 ; i--){
+//   let digit = n % 10;
+//   sum += digit;
+//   n = parseInt(n / 10)
+// }
+
+// console.log(sum);
+
+
+
+
+
+
+
+
+
+// 4 - masala
+// let akmal = 70;
+
+// console.log('Akmal 1 soat ichida '+ akmal*60 + 'metr yuguradi');
+
+
+
+
+
+// 6 - masala
+
+// let cost = prompt('Mahsulot summasini kiriting');
+// let vazn = prompt('Mahsulot vaznini kiriting');
+
+// console.log(cost*vazn);
+
+
+
+
+
+// 7 - masala
+
+// var n = 123, reverse = 0, remainder;
+
+// while (n>0) {
+//     remainder = n%10;
+//     reverse = reverse * 10 + remainder;
+//     n = Math.floor(n / 10);
+// }
+
+// n = parseInt(n / 10);
+
+// console.log(reverse);
+
+
+
+
+
+
+//  8 - masala
+
+// let benzin = prompt('Mashinangiz qancha benzin sarf qiladi? ')
+
+// console.log();
+
+
+
+
+
+
+// 9 - masala
+
+// let user = prompt('Tugilgan yilingizni kiriting')
+
+// let yosh = 2023 - user
+
+// console.log('Siz '+yosh + ' yoshsiz');
+
+// uyga vazifa 1 - masala
+
+// let son = Number(prompt('Son kiriting'));
+// let son1 = Number(prompt('2chi sonni kiriting'));
+
+// let q = son + son1;
+// let a = son -son1;
+// let k = son *son1;
+// let b = son / son1;
+
+// console.log(`Sonlar yig'indisi : ${q}
+// Ayirmasi : ${a}
+// Ko'paytmasi : ${k}
+// Bo'linmasi : ${b}
+//  `);
+
+
+
+
+
+// 3 - masala
+
+// const num = 123;
+// const reverse = (num) => parseInt(String(num)
+// .split("")
+// .reverse()
+// .join(""), 10);
+// console.log(reverse(num));
+
+
+
+
+
+
+//  6 - masala
+
+// let son = Number(prompt('son kiriting'));
+
+// if (son%2 === 0) {
+//     console.log('Juft son');
+// }
+// else if (son%2 === 1) {
+//   console.log('Toq son');
+// }
+// else{
+//   console.log('Iltimos son kiriting!!!');
+// }
+
+
+
+
+
+
+
+
+// 7 - masala
+
+// let a = prompt('a sonni kiriting');
+// let b = prompt('b sonni kiriting');
+// let c = prompt('c sonni kiriting');
+// let d = prompt('d sonni kiriting');
+
+// if (a>0 && b>0 && c>0 && d>0 ) {
+//     console.log('Hamma sonlar musbat ');
+// }
+// else if(a<0 || b<0 || c<0 || d<0){
+//     console.log('Hamma sonlar musbat emas!!! ');
+// }
+// else{
+//   console.log('Faqat son kiriting!!! ');
+// }
+
+
+
+
+
+
+
+// 8 - masala
+
+// let age = prompt('Yoshni kirit')
+// let n = prompt('Ismni kirit')
+
+// if (age<=0) {
+//     console.log('Togri malumot kiriting');
+// }
+// else if (age>0){
+//   console.log(`Siz ${age} yoshsiz`);
+// }
+// if (n === String()) {
+//     console.log(`salom ${n}`);
+// }
+
+// console.log(202030.2393.toFixed(2));
+
+
+
+
+
+// 10 - masala
+
+// let n = 'Anvar';
+// let y = 21;
+// let j = 'doctor';
+
+// if (n === 'Akmal' || y === 21 || j === 'dasturchi') {
+// 	console.log(`Siz to'g'ri ma'mulot kiritdingiz`);
+// }
+// else{
+//   console.log(`Iltimos to'g'ri ma'lumot kiriting!!!`);
+// }
+
+
+
+
+
+
+// #1 - masala
+
+// let a = 89;
+// let b = 8;
+// let c = 23;
+
+// if (a > b && a >c) {
+// 	console.log(`Eng katta son ${a}`);
+// }
+// else if (b > a && b>c){
+//     console.log(`Eng katta son ${b}`);
+// }
+// else if (c>a && c>b){
+// 	console.log(`Eng katta son ${c}`);
+// }
+// else if(a>=b || a<=c || b>=c || b>=a){
+//   console.log('Sonlar teng');
+// }
+
+
+
+
+
+
+// #2 - masala
+
+// let a = 89;
+// let b = 8;
+// let c = 23;
+
+// if (a < b && a <c) {
+// 	console.log(`Eng kichik son ${a}`);
+// }
+// else if (b < a && b<c){
+//     console.log(`Eng kichik son ${b}`);
+// }
+// else if (c<a && c<b){
+// 	console.log(`Eng kichik son ${c}`);
+// }
+// else if(a>=b || a<=c || b>=c || b>=a){
+//   console.log('Sonlar teng');
+// }
+
+
+
+
+
+
+// 3 - masala
+
+// let money = Number(prompt('Qancha  pulingiz  bor'))
+
+// if (money >= 1000) {
+// 	console.log('Lux xona olishingiz mumkin');
+// }
+// else if(money >= 500){
+//    console.log('Half lux xona olishingiz mumkin');
+// }
+// else if(money >= 200){
+//    console.log('Oddiy xona olishingiz mumkin');
+// }
+// else if (money < 200){
+//   console.log('Joy yo\'q');
+// }
+// else{
+//    console.log(`To'g'ri malumot kirting`);
+// }
+
+
+
+
+
+// 4 -masala
+
+// let num = Number(prompt('Son kiriting'));
+
+// if (num%5 === 0 && num%3 === 0) {
+// 	console.log('fizz-bazz');
+// }
+// else if (num%3 === 0  ) {
+// 	console.log('FIZZ');
+// }
+// else if (num%5 === 0){
+//    console.log('BAZZ');
+// }
+// else{
+//   console.log(`${num} 3ga ham 5ga ham bo'linmaydi`);
+// }
+
+
+
+
+
+// IF ELSE
+
+// **Masala 2**
+
+// let age = prompt('How old r u ?')
+
+// if (age>=18) {
+// 	console.log('Xush kelibsiz');
+// }
+// else{
+//    console.log('Uzur ukam');
+// }
+
+
+
+
+
+
+// 3 - masala
+
+// let x = 9;
+// let y = 71;
+
+// if (x>y) {
+// 	console.log(` ${x} > ${y}`);
+// }
+// else if (x<y){
+//     console.log(`${x} < ${y}`);
+// }
+// else{
+//    console.log(`${x} = ${y}`);
+// }
+
+
+
+
+
+// 4 - masala
+
+// let a = 43;
+// let b = -96;
+
+// if (a>0 && b>0) {
+// 	console.log(`${a} va ${b} sonlar musbat`);
+// }
+// else if(a<0 && b<0)
+// {
+// 	console.log(`${a} va ${b} sonlar manfiy`);
+// }
+// else if(a>0 && b<0){
+//    console.log(`${a} soni musbat \n${b} soni manfiy`);
+// }
+// else if(a<0 && b>0){
+// 	console.log(`${a} soni manfiy \n${b} soni musbat`);
+//  }
+// else{
+//    console.log('Faqat son kiriting');
+// }
+
+
+
+
+
+
+// masala 1-kalkulator
+
+// let son1 = prompt('1-son');
+// let son2 = prompt('2-son');
+// let q = son1 + son2
+// let a = son1 - son2;
+// let k = son1 * son2;
+// let b = son1 / son2;
+
+// if (son1 + son2 ) {
+// 	console.log(`${son1} + ${son2} = ${q}` );
+// }
+
+
+
+
+
+//  2 - M
+
+// let kun = Number(prompt('7 gacha bo\'lgan son kirting'));
+
+// if (kun === 1) {
+// 	console.log('Dushanba');
+// }
+// else if(kun === 2){
+//    console.log('Seshanba');
+// }
+// else if(kun === 3){
+//    console.log('Chorshanba');
+// }
+// else if(kun === 4){
+//    console.log('Payshanba');
+// }
+// else if (kun === 5) {
+// 	console.log('Juma');
+// }
+// else if (kun === 6) {
+// 	console.log('shanba');
+// }
+// else if (kun === 7){
+//    console.log('yakshaba');
+// }
+// else{
+//    console.log(`to'g'ri ma'lumot kirting`);
+// }
+
+
+
+
+
+
+// 3 - M
+
+// let num = Number(prompt('Son kiriting'));
+
+// if (num>7) {
+// 	console.log(`${num} > 7`);
+// }
+// else if(num<7){
+//    console.log(`${num} < 7`);
+// }
+// else if(num === 7){
+//    console.log(`${num} = 7`);
+// }
+// else{
+//    console.log('Faqat son k');
+// }
+
+
+
+
+
+
+
+
+// 4 - M
+
+// let raqam = Number(prompt('Son'));
+
+// if (raqam>0) {
+// 	console.log(--raqam);
+// }
+// else if (raqam<0){
+//     console.log(++raqam);
+// }
+// else if (raqam === 0){
+//    console.log(0);
+// }
+// else{
+//    console.log(`Please try again`);
+// }
+
+
+
+
+
+
+
+// 5 - M
+
+// let ball = Number(prompt('Ball nech????'))
+
+// let result = ((ball*100)/200);
+
+// program to check if a number is prime or not
+
+// take input from the user
+// const number = parseInt(prompt("Enter a positive number: "));
+// let isPrime = true;
+
+// // check if number is equal to 1
+// if (number === 1) {
+//     console.log("1 is neither prime nor composite number.");
+// }
+
+// // check if number is greater than 1
+// else if (number > 1) {
+
+//     // looping through 2 to number-1
+//     for (let i = 2; i < number; i++) {
+//         if (number % i == 0) {
+//             isPrime = true;
+
+//         }
+//     }
+
+//     if (isPrime) {
+//         console.log(`${number} is a prime number`);
+//     } else {
+//         console.log(`${number} is a not prime number`);
+//     }
+// }
+
+// // check if number is less than 1
+// else {
+//     console.log("The number is not a prime number.");
+// }
+
+
+
+
+
+
+
+
+// //  function declaration
+// function add(params) {
+
+// }
+
+// //
+
+// let narh = 5000;
+// let kg = 4;
+
+// for (let i = 1; i <= kg; i++) {
+//     console.log(`${i} kg = ${i*narh}`);
+
+// }
+
+
+
+
+
+
+// calculator
+
+// let son1 = Number(prompt('1chi son'));
+// let amal = prompt('Amalni kiriting');
+// let son2 = Number(prompt('2chi son'));
+
+// if (amal == '+') {
+//     console.log(`${son1} + ${son2} = ${son1 + son2}`);
+// }
+// else if(amal == '-'){
+//     console.log(`${son1} - ${son2} = ${son1 - son2}`);
+// }
+// else if (amal == '*'){
+//      console.log(`${son1} * ${son2} = ${son1 * son2}`);
+// }
+// else if (amal == '/'){
+//     console.log(`${son1} / ${son2} = ${son1 / son2}`);
+// }
+// else{
+//    console.log(`Iltimos to'g'ri ma'lumot kiriting`);
+// }
+
+
+
+
+
+
+
+// let sign = prompt("What's your profession?");
+
+// if (sign == "student") {
+//   alert("Wow! I'm a Student too!");
+// }
+
+// let sum = 0;
+// const n = 100
+
+// // looping from i = 1 to n
+// // in each iteration, i is increased by 1
+// for (let i = 1; i <= n; i++) {
+//     sum += i;  // sum = sum + i
+// }
+
+// console.log('sum:', sum);
+
+
+
+
+
+
+// for(let i = 10; i >= 0; i--) {
+// 	console.log(i);
+// }
+
+
+
+
+
+
+// let htmlPassed = false;
+// let cssPassed = false;
+// let message = '';
+
+// if (htmlPassed && cssPassed) {
+// 	message = 'Siz Bootstrap kursini boshlashingiz mumkin'
+// }
+// else if (htmlPassed || cssPassed){
+// 	message = 'Siz kursni to\'liq tugatmagansiz!!!'
+// }
+// else {
+//   message = 'Iltimos birinchi ikkala kursni tugatib chiqing!!!'
+// }
+
+// console.log(message);
+
+
+
+
+
+
+
+
+// calendar
+
+// son = 2;
+
+// switch (3) {
+//     case 1:
+//         console.log('Dushanba');
+//         break;
+//     case 2:
+//         console.log('Seshanba');
+//         break;
+//     case 3:
+//         console.log('Chorshanba');
+//         break;
+//     case 4:
+//         console.log('Payshanba');
+//         break;
+//     case 5:
+//         console.log('Juma');
+//         break;
+//     case 6:
+//         console.log('shanba');
+//         break;
+//     case 7:
+//         console.log('Yakshanba');
+//         break;
+//     default:
+//         console.log("Bunaqa kun yo'q");
+//         break;
+// }
+
+
+
+
+
+
+// for(let i = 0; i<10; i++){
+//    if (i == 6) {
+//      break;
+//    }
+//    console.log(i);
+// }
+
+
+
+
+
+// let str = '2jd3dk'
+
+// for (let i = 0; i < str.length; i++) {
+//   console.log(str[i]);
+//     }
+
+// let str = 'salom';
+// let str2 = '';
+
+// for (let i = str.length-1; i >=0; i--) {
+//     console.log(str[i]);
+
+// }
+
+// let i = 1;
+// const n = 5;
+
+// // do...while loop from 1 to 5
+// do {
+//     console.log(i);
+//     i++;
+// } while(i <= n);
+
+// // first loop
+// for (let i = 1; i <= 3; i++) {
+
+//     // second loop
+//     for (let j = 1; j <= 3; j++) {
+//         if (i == 2) {
+//           break;
+//         }
+//         console.log(`i = ${i}, j = ${j}`);
+//     }
+// }
+
+
+
+
+
+
+
+// bankomat
+
+// let parol = 1234;
+// let sum = 15000;
+// let imkon = 3;
+
+// while (true) {
+//     let parol2 = prompt('Please parol kiriting');
+//     if (parol2 == parol) {
+//         while (true) {
+//         let variant = prompt(` 1 hisob tekshirish \n 2 pul olish \n 3 xizmatni tugatish`);
+//         if (variant == 1) {
+//         alert(`hisobingizda ${sum} $ bor`)
+//         }
+//         else if (variant == 2){
+//           let pul = +prompt(`olmoqchi bo'lgan summangizni kiriting sizni kartangizda ${sum} $ bor`);
+
+//           if (sum > pul) {
+//             sum = sum - pul;
+//             alert(`hisobingizda ${sum} $ qoldi`)
+//           }
+//           else{
+//             alert(`hisobingizda mablag' yetarli emas`)
+//           }
+//         }
+//         else if (variant == 3){
+//            break;
+//         }
+
+//         }
+//     }
+//     else{
+//       imkon-=1
+//       if (imkon == 0) {
+//         alert(`karta sizniki emas`)
+//         break;
+//        }
+//       alert(`parol noto'g'ri sizda ${imkon} imkon bor`)
+//     }
+
+// }
+
+// let son = prompt('Son kiriting');
+
+
+
+
+
+// function num() {
+//     return son= son **3
+// }
+// console.log(num(5));
+
+// function sum(a, b) {
+//     return a + b
+// }
+// console.log(sum(12,12));
+
+// function subtractionNumber(x, y) {
+//    return x - y
+// }
+// console.log(subtractionNumber(prompt('son1'), prompt('son2')));
+
+
+
+
+
+
+// function x(a) {
+//       let son = `${a**2} \n${a**3} \n${a**4}`
+//       return son;
+// }
+// console.log(x(6));
+
+
+
+
+
+// let son = 101
+// function toq(a) {
+//     for (let i = 1;  i < a; i++) {
+//         if (i%2 != 0) {
+//             console.log(i);
+//         }
+//     }
+//     return a
+// }
+// console.log(toq(son));
+
+
+
+
+
+// function indic(a) {
+//     let inc = `${--a} \n${a+=2}`;
+//     return inc
+// }
+// console.log(indic(78));
+
+
+
+
+
+// function n() {
+//     for (let i = 1; i < 10; i++) {
+//         for (let l = 1; l < 10; l++) {
+//             console.log(`${i} * ${l} = ${i * l}`);
+
+//         }
+
+//     }
+
+// }
+// console.log(n());
+
+
+
+
+
+// function greet(name) {
+//     console.log("Hello " + name + ":)");
+// }
+
+// let name = prompt("Enter a name: ");
+
+// greet(name);
+
+
+
+
+
+
+
+// function math(x,y) {
+//     console.log(`${x} * ${y} = ${x * y}`);
+//     console.log(`${x} / ${y} = ${x / y}`);
+//     console.log(`${x} - ${y} = ${x - y}`);
+//     console.log(`${x} + ${y} = ${x + y}`);
+// }
+
+// console.log(math (Number(prompt('1- sonni kiriting')), Number(prompt('2 - sonni kiriting'))));
+
+
+
+
+
+
+
+
+// let str = '1d2dsl37fd39'
+// let numResult = 0;
+// let str2 = ''
+
+// for (let i = 0; i < str.length; i++) {
+//     if (parseInt(str[i])) {
+//         numResult += parseInt(str[i]);
+//     }
+//     else{
+//        str2 += str[i]
+//     }
+
+// }
+// console.log(numResult, str2);
+
+
+
+
+
+
+
+
+// function son(a) {
+//     if (a % 2 == 0) {
+//         console.log(`${a} juft son`);
+//     }
+//     else{
+//         console.log(`${a} toq son`);
+//     }
+// }
+
+// son(200);
+
+
+
+
+
+
+
+// let son = function (a) {
+//     if (a % 2 == 0) {
+//         console.log(`${a} juft son`);
+//     }
+//     else{
+//         console.log(`${a} toq son`);
+//     }
+// }
+
+// son(3);
+
+
+
+
+
+
+
+// let son = (a) => {
+//     if (a % 2 == 0) {
+//         console.log(`${a} juft son`);
+//     }
+//     else{
+//         console.log(`${a} toq son`);
+//     }
+// }
+
+// son(7100);
+
+
+
+
+
+
+// function maxExp(a,b) {
+//     if (a >= b) {
+//         return a;
+//     }
+//     else{
+//         return b;
+//     }
+// }
+
+
+
+
+
+
+
+// console.log(maxExp(1,87));
+
+// function reversedDec(number) {
+//     const numberStr = String(number);  "56"
+//     const reversedStr = numberStr.split('').reverse().join('');
+//     // ["5", "6"], ["6", "5"],   str yani "65"
+//     return Number(reversedStr)
+// }
+
+// console.log(reversedDec(13));
+
+
+
+
+
+
+
+
+
+//  8 -masala
+
+// program to find the factors of an integer
+
+// take input
+
+// const num = prompt('Enter a positive number: ');
+
+// console.log(`The factors of ${num} is:`);
+
+// // looping through 1 to num
+// for(let i = 1; i <= num; i++) {
+
+//     // check if number is a factor
+//     if(num % i == 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+
+
+
+// Object
+
+// let obj = {
+//     name: "Ahmed",
+//     age: 20,
+//     job: "Developer",
+//     language: {
+//         eng: "english",
+//         uz: "uzbek",
+//     },
+// };
+// console.log(obj.name);
+
+
+
+
+
+
+// function myFunc(name, year) {
+//     return `Salom mening ismim ${name}, yoshim ${2023-year} da`
+// }
+
+// console.log(myFunc('Mirjalol', 2004));
+
+
+
+
+
+// let aSon = +prompt('a sonini kiriting', 0);
+// let bSon = +prompt('b sonini kiriting', 0);
+// let cSon = +prompt('c sonini kiriting', 0);
+// let dSon = +prompt('d sonini kiriting', 0);
+
+// function name(params) {
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+// pure function  tashqaridagi  o'zgaruchini qiymatini  ozgartirmaydigan funksiyalar  pure func deyiladi  
+
+// arrowni dec funcdan  3ta farqi  yozilishida, arguments key yoq arrowda, this i yoq
+
+
+// let a = 5;
+
+
+// function fn() {
+//     a = 20;
+// }
+
+// fn()
+
+// console.log(a);
+
+
+// const x = () => {
+    
+// }
+
+
+
+
+
+
+// function x(a) {
+//     if (a == 0) {
+//         return 'salom';
+//     }
+
+//     return x(a - 1)
+// }
+
+// console.log(x(5));
+
+
+
+
+
+
+
+// program to find the factorial of a number
+   
+// function factorial(x) {
+
+//     // if number is 0
+//     if (x === 0) {
+//         return 1;
+//     }
+
+//     // if number is positive
+//     else {
+//         return x * factorial(x - 1);
+//     }
+// }
+
+// console.log(factorial());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
